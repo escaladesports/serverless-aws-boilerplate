3e8ed706846ec872ae17d0161e1eb88cf00be146
Kennedy Rose <kennedyianrose@gmail.com>
2018-08-10 11:16:47 -0500

    Proper export

--- src/handler.js
+++ src/handler.js
@@ -1,16 +1,18 @@
-'use strict';
 
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+export function hello(event, context, callback){
+	console.log(`Endpoint hit...`)
+	const response = {
+		statusCode: 200,
+		headers: {
+			'Access-Control-Allow-Origin': `*`,
+			'Access-Control-Allow-Credentials': true,
+		},
+		body: JSON.stringify({
+			message: `Go Serverless v1.0! Your function executed successfully!`,
+		}),
+	}
 
-  callback(null, response);
+	console.log(response)
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
-};
+	callback(null, response)
+}
